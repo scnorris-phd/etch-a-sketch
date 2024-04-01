be1acfce025502af997b2ef9779187dda865082c
scnorris-phd <159732029+scnorris-phd@users.noreply.github.com>
2024-04-01 15:52:49 +1030

    Change default grid to black and white and add color toggle functionality

--- javascript.js
+++ javascript.js
@@ -26,7 +26,7 @@ function createGrid(num) {
                 cell.style.flex = "1";
                 function colorChange(event) {
                     if (event.ctrlKey) {
-                        cell.style.backgroundColor = getRandomColor();
+                        cell.style.backgroundColor = "black";
                     }
                 }
                 cell.addEventListener("mouseover", () => {
@@ -63,3 +63,56 @@ function newGrid() {
 
 const newGridButton = document.getElementById("newGridBtn");
 newGridButton.addEventListener("click", newGrid);
+
+function colorGrid(num) {
+    for (let i = 1; i <= num; i++) {
+        const row = document.createElement("div");
+        container.appendChild(row);
+        row.style.display = "flex";
+        row.style.flex = "1";
+        row.style.margin = "0";
+
+        function createCell(a) {
+            for (let j = 1; j <= a; j++) {
+                const cell = document.createElement("div");
+                cell.style.border = "1px solid black";
+                cell.style.margin = "0";
+                cell.style.backgroundColor = "white";
+                cell.style.flex = "1";
+                function colorChange(event) {
+                    if (event.ctrlKey) {
+                        cell.style.backgroundColor = getRandomColor();
+                    }
+                }
+                cell.addEventListener("mouseover", () => {
+                    colorChange(event);
+                });
+                row.appendChild(cell);
+            }
+        }
+
+        createCell(num);
+    }    
+}
+
+function newColorGrid() {
+    let newGridSize = prompt("Enter a number between 2 and 100 to change the size of the grid cells. Invalid entries will return the default grid.");
+
+    if (newGridSize > 100) {
+        newGridSize = 100;
+    } else if (newGridSize < 2) {
+        newGridSize = 16;
+    } else if (newGridSize >= 2 && newGridSize <= 100) {
+        newGridSize;
+    } else {
+        newGridSize = 16;
+    }
+
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+    colorGrid(newGridSize);
+}
+
+const colorBtn = document.getElementById("colorBtn");
+colorBtn.addEventListener("click", newColorGrid);
